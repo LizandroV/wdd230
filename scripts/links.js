90b86098f5580975dea0c0be0b828e3f782b7dfa
LizandroV <lizandrovivanco16@gmail.com>
2024-03-07 17:48:19 -0500

    adding JSON to index

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,26 @@
 const baseURL = "https://lizandrov.github.io/wdd230/";
 const linksURL = "https://lizandrov.github.io/wdd230/data/links.json";
 
+const activities = document.querySelector("#learning");
+
 async function getLinks() {
     const response = await fetch(linksURL);
     const data = await response.json();
-    console.log(data);
-  }
-  
-  getLinks();
+    console.log(data)
+    displayLinks(data);
+}
+
+const displayLinks = (weeks) => {
+    weeks.forEach((week) => {
+        // Create elements to add to the div.cards element
+        let list = document.createElement('li');
+
+        // Build the h2 content out to show the prophet's full name
+        list.textContent = `${week.lesson}`;
+
+        // Append the section(card) with the created elements
+        activities.appendChild(list);
+    });
+}
+
+getLinks();
