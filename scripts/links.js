4c48435eae3b5241eadda88a3ed621eaf29d3fac
LizandroV <lizandrovivanco16@gmail.com>
2024-03-08 10:29:31 -0500

    My home page is working with a JSON File

--- scripts/links.js
+++ scripts/links.js
@@ -6,24 +6,23 @@ const linksURL = "https://lizandrov.github.io/wdd230/data/links.json";
 const linkBox = document.getElementById('link-box');
 
 const getLinks = async function() {
-    const response = await fetch(linksUrl);
+    const response = await fetch(linksURL);
     const data = await response.json();
     // console.log(data);
-    displayLinks(data.weeks);
+    displayLinks(data.lessons);
 }
 
 const displayLinks = function(data) {
     for (let index = 0; index < data.length; index++) {
         let listEl = document.createElement('li');
 
-        listEl.innerHTML = `${data[index].week}: `;
+        listEl.innerHTML = `${data[index].lesson}: `;
         data[index].links.forEach((link, i) => {
             let linkEl = document.createElement('a');
             linkEl.href = link.url;
             linkEl.title = link.title;
             linkEl.innerHTML = link.title;
             linkEl.target = '_blank';
-
             listEl.appendChild(linkEl)
         });
 
