6aee15cef7c0d0508c41c41ed3de8f4158623015
LizandroV <61571830+LizandroV@users.noreply.github.com>
2024-03-16 19:30:24 -0500

    adding the banner

--- chamber/scripts/slide.js
+++ chamber/scripts/slide.js
@@ -12,3 +12,21 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  var currentDate = new Date();
+  var currentDay = currentDate.getDay(); // 0 is Sunday, 1 is Monday, and so on.
+
+  // Show the banner only on Mondays, Tuesdays, and Wednesdays
+  if (currentDay >= 1 && currentDay <= 3) {
+      var banner = document.getElementById('banner');
+      banner.style.display = 'block';
+  }
+
+  // Close banner functionality
+  var closeBannerBtn = document.getElementById('closeBanner');
+  closeBannerBtn.addEventListener('click', function() {
+      var banner = document.getElementById('banner');
+      banner.style.display = 'none';
+  });
+});
--- scripts/windchill.js
+++ scripts/windchill.js
@@ -2,26 +2,43 @@ const api = {
     key: "0d3d43ff64c667b6eb270ea5a34612b5",
     url: "https://api.openweathermap.org/data/2.5/weather"
 }
+const weather = document.getElementById("current");
+const outside = document.getElementById("outside");
+const wind = document.getElementById("wind");
+const weatherIcon = document.querySelector("#weather-icon");
 
 async function getWeather(city){
     try{
         const response = await fetch(`${api.url}?q=${city}&appid=${api.key}&lang=en`)
-        const data = await response.json();
-
-        weather.innerHTML = toCelsius(data.main.temp);
-        outside.innerHTML = data.weather[0].description;
-        wind.innerHTML = data.wind.speed;
+        if (response.ok){
+            const data = await response.json();
+            console.log(data);
+            displayResults(data);
+        }
+        else{
+            throw Error(await response.text());
+        }
     }   
-    catch(err){
-        //console.log(err);
+    catch(error){
+        console.log(err);
     }
+}
+function displayResults(data){
+    weather.innerHTML = `${toCelsius(data.main.temp)}`;
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    let desc = data.weather[0].description
+    outside.innerHTML = `${desc}`;
+    wind.innerHTML = data.wind.speed;
+    weatherIcon.setAttribute("src", iconsrc);
+    weatherIcon.setAttribute("alt", desc);
+    
 }
 
 getWeather("Lima")
 
-const weather = document.getElementById("current");
-const outside = document.getElementById("outside");
-const wind = document.getElementById("wind");
+
+
+
 
 function toCelsius(kelvin){
     return Math.round(kelvin - 273.15);
